956bc42b755a72bb9eee9b0268e5dc0094e49875
b3rking <ishimweeschylebersy@gmail.com>
2021-11-30 21:22:51 +0200

    feat: fetching all users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller {
 
-    public function index() {
-        $data = User::find(1);
-        return response()->json($data, 200);
+    public function index(Request $request) {
+        $data = User::all();
+        return response()->json([
+            "status" => 200,
+            "endpoint" => $request->fullUrl(),
+            "msg" => "ok",
+            "response" => $data], 200);
     }
 }
